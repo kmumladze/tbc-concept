5ab46e207135640da903dc7ea3ea9c2a630ad915
Ketevan Mumladze <ketevanmumladze17@gmail.com>
2024-07-22 12:59:28 +0400

    feat: comleted dropdown menu

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
+const dropdownContainer = document.querySelector(".card-dropdown-menu");
+const firstDropDown = document.querySelector(".first-dropdown-menu");
+const secondDropDown = document.querySelector(".second-dropdown-menu");
+const thirdDropDown = document.querySelector(".third-dropdown-menu");
+
 function toggleDropDown() {
-  const dropdownContainer = document.querySelector(".card-dropdown-menu");
-  const firstDropDown = document.querySelector(".first-dropdown-menu");
   dropdownContainer.classList.toggle("show");
   firstDropDown.classList.toggle("show");
+}
+function toggleDropDown2() {
+  dropdownContainer.classList.toggle("show");
+  secondDropDown.classList.toggle("show");
+}
+function toggleDropDown3() {
+  dropdownContainer.classList.toggle("show");
+  thirdDropDown.classList.toggle("show");
 }
