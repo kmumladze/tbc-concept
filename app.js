fa35689f8ce5ea2bd72a75306bb0749d25f3e8af
Ketevan Mumladze <ketevanmumladze17@gmail.com>
2024-07-23 12:05:46 +0400

    feat: completed first section

--- app.js
+++ app.js
@@ -5,13 +5,54 @@ const thirdDropDown = document.querySelector(".third-dropdown-menu");
 
 function toggleDropDown() {
   dropdownContainer.classList.toggle("show");
+  if (dropdownContainer.classList.contains("show")) {
+    dropdownContainer.classList.remove("show");
+  }
+  removeclass();
+
   firstDropDown.classList.toggle("show");
+
+  if (secondDropDown.classList.contains("show")) {
+    secondDropDown.classList.remove("show");
+    secondDropDown.classList.add("hide");
+  }
+  if (thirdDropDown.classList.contains("show")) {
+    thirdDropDown.classList.remove("show");
+    thirdDropDown.classList.add("hide");
+  }
+  //   thirdDropDown.classList.add("hide");
 }
 function toggleDropDown2() {
   dropdownContainer.classList.toggle("show");
+  removeclass();
+
+  if (firstDropDown.classList.contains("show")) {
+    firstDropDown.classList.remove("show");
+    firstDropDown.classList.add("hide");
+  }
+  if (thirdDropDown.classList.contains("show")) {
+    thirdDropDown.classList.remove("show");
+    thirdDropDown.classList.add("hide");
+  }
+
   secondDropDown.classList.toggle("show");
 }
 function toggleDropDown3() {
   dropdownContainer.classList.toggle("show");
+  removeclass();
   thirdDropDown.classList.toggle("show");
+  if (secondDropDown.classList.contains("show")) {
+    secondDropDown.classList.remove("show");
+    secondDropDown.classList.add("hide");
+  }
+
+  if (firstDropDown.classList.contains("show")) {
+    firstDropDown.classList.remove("show");
+    firstDropDown.classList.add("hide");
+  }
+
+  //   firstDropDown.classList.add("hide");
+}
+function removeclass() {
+  dropdownContainer.classList.remove("card-dropdown-menu");
 }
